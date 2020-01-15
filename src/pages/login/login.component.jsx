ee9e6e46e46f1439d28de78517248a5c5be071f0
Tiago Lopes de Paiva Dionysio da Fonseca <tiagoapolo@Tiagos-MacBook-Pro.local>
2020-01-15 09:43:15 -0300

    dev bottom

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './login.scss';
 import LoginForm from '../../components/login-form/login-form.component';
@@ -6,11 +7,18 @@ import Logo from '../../components/logo/logo.component';
 
 function Login () {
 
+  let history = useHistory();
+
+  const goToInfo = () => history.push('/developer')
+
   return (
     <div className="Login">
       <div className="container">
         <Logo/>
         <LoginForm/>
+      </div>
+      <div className="bottom">
+        <p onClick={goToInfo} className="contact">Contato do desenvolvedor</p>
       </div>
     </div>
   )
